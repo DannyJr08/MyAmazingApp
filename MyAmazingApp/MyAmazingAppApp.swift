5cdb0fa090a033f53f1edc6f381f86de4e9f0a2d
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2024-11-04 15:19:08 +0100

    Initial Commit

---
+++ MyAmazingApp/MyAmazingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmazingAppApp.swift
+//  MyAmazingApp
+//
+//  Created by Juan Daniel Rodriguez Oropeza on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAmazingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
